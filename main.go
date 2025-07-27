85bdee3f80433f632ba3f8bdb1ba8d256bfc13a4
kissy24 <kissy105808j@gmail.com>
2025-07-27 12:18:37 +0900

    feat: Supported pagination

--- main.go
+++ main.go
@@ -23,6 +23,24 @@ func CreateGHContents() (context.Context, *github.Client) {
 	return ctx, client
 }
 
+// getAllUsers retrieves the complete list of followers or following for the specified user.
+func getAllUsers(ctx context.Context, client *github.Client, userName string, listFunc func(context.Context, string, *github.ListOptions) ([]*github.User, *github.Response, error)) ([]*github.User, error) {
+	var allUsers []*github.User
+	opt := &github.ListOptions{Page: 1, PerPage: 100} // Increase the number of items per page
+	for {
+		users, resp, err := listFunc(ctx, userName, opt)
+		if err != nil {
+			return nil, err
+		}
+		allUsers = append(allUsers, users...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+	return allUsers, nil
+}
+
 type FollowStatus struct {
 	followings []*github.User
 	followers  []*github.User
@@ -49,15 +67,15 @@ func (fs FollowStatus) CompareUsers() []*github.User {
 func main() {
 	userName := "kissy24" // Change your user name.
 	ctx, client := CreateGHContents()
-	followers, resp, err := client.Users.ListFollowers(ctx, userName, nil)
+	followers, err := getAllUsers(ctx, client, userName, client.Users.ListFollowers)
 	if err != nil {
-		fmt.Println(resp.Body)
-		fmt.Println(err)
+		fmt.Printf("An error occurred while acquiring followers: %v\n", err)
+		os.Exit(1)
 	}
-	followings, resp, err := client.Users.ListFollowing(ctx, userName, nil)
+	followings, err := getAllUsers(ctx, client, userName, client.Users.ListFollowing)
 	if err != nil {
-		fmt.Println(resp.Body)
-		fmt.Println(err)
+		fmt.Printf("An error occurred while acquiring the following: %v\n", err)
+		os.Exit(1)
 	}
 	fs := FollowStatus{followings, followers}
 	notFollowingBack := fs.CompareUsers()
